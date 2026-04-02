d7f8c1cb79309f3479530cd69746ce1a4e036bdd
kunalahirrao03 <kunalahirrao555@gamil.com>
2026-04-03 04:09:25 +0530

    model add in app.py

--- app/app.py
+++ app/app.py
@@ -2,3 +2,9 @@ import os
 import streamlit as st
 import numpy as np
 import pickle
+# --- LOAD MODEL --- #
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "model.pkl")
+
+with open(model_path, "rb") as f:
+    w, b, mean, std, y_mean, y_std = pickle.load(f)
